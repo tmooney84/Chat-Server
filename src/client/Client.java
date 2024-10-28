378c973177499592fc505ab56fe278500914e94f
Trevor Mooney <trevordmooney@gmail.com>
2024-10-28 13:35:54 -0700

    ServerThread created

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,15 +2,20 @@ package client;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         Socket socket = null;
+        System.out.println("Please input username:");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        scan.close();
         int portNumber = 4444;
         try{
             socket = new Socket("localhost", portNumber);
             Thread.sleep(1000);
-            Thread server = new Thread(new ServerThread(socket));
+            Thread server = new Thread(new ServerThread(socket,name));
             server.start();
 
         } catch(IOException e){
--- src/client/ServerThread.java
+++ src/client/ServerThread.java
@@ -1,16 +1,44 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ServerThread implements Runnable {
     private Socket socket;
+    private String name;
+    private BufferedReader serverIn;
+    private BufferedReader userIn;
+    private PrintWriter out;
 
-    public ServerThread(Socket socket) {
+    public ServerThread(Socket socket, String name) {
         this.socket = socket;
+        this.name = name;
     }
 
     @Override
     public void run(){
-        // TODO make thread relay messages
+        try {
+          out = new PrintWriter(socket.getOutputStream(),true);
+          serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          userIn = new BufferedReader(new InputStreamReader(System.in));
+
+          //while the socket is still alive
+            while(!socket.isClosed()){
+                if(serverIn.ready()) {
+                    String input = serverIn.readLine();
+                    if(input != null){
+                        System.out.println(input);
+                    }
+                }
+                if(userIn.ready()){
+                    out.println(name+" > " + userIn.readLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
