3297d759d1edc27893befa67b3378832857c94b3
Trevor Mooney <trevordmooney@gmail.com>
2024-10-28 13:04:31 -0700

    client thread changes

--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClientThread extends ChatServer implements Runnable{
     private Socket socket;
@@ -19,6 +17,19 @@ public class ClientThread extends ChatServer implements Runnable{
     @Override
     public void run() {
         try {
+            /*
+            socket.getOutputStream() retrieves the raw OutputStream associated with this
+            specific socket. This OutputStream is a low-level, byte-based stream that allows
+            data to be sent from the server to the client. However, handling raw bytes is
+            inconvenient for text-based communication, so we wrap this OutputStream with a
+            PrintWriter which provides convenient methods for sending formatted text data.
+            The second parameter, true, enables auto-flushing for the PrintWriter. When
+            auto-flush is enabled, any call to println(), printf(), or format() will
+            automatically flush the stream, sending data immediately to the client. Why
+            Auto-Flush Matters: In a chat server, real-time communication is essential.
+            By setting autoFlush to true, the server ensures that messages are sent to the
+            client without unnecessary delays.
+            */
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
